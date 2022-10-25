620bae9e558750a4adbbab384f8a43fbe2778d8b
Kade Illian <kadeillianmt@gmail.com>
2022-10-25 10:59:23 -0600

    bootstrap is not working, but images can now be rendered to the modal without breaking it

--- src/PlacesShow.jsx
+++ src/PlacesShow.jsx
@@ -1,6 +1,7 @@
 export function PlacesShow(props) {
   return (
-    <div id="places-show">
+    <div>
+      <img src={props.place.image_url} className="modal-image" />
       <h1>{props.place.name}</h1>
       <h3>{props.place.country}</h3>
       <h5>This is where the lat long will go. Figure out how to make lat on the right and long on the left</h5>
