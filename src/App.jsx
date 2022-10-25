f9a0e80063048f6695219ac1292b5aaff086821d
Kade Illian <kadeillianmt@gmail.com>
2022-10-25 12:46:28 -0600

    experimenting with gradient background and added a favicon and changed the website title

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { Header } from "./Header";
 import { Home } from "./Home";
 import { Footer } from "./Footer";
+import "./App.css";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Home />
-      <Footer />
+    <div className="app-css">
+      <Container>
+        <Header />
+        <Home />
+        <Footer />
+      </Container>
     </div>
   );
 }
