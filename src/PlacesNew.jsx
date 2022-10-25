e0a9c04cdfba6483b214d6d88e7bac3afc6a2a53
Kade Illian <kadeillianmt@gmail.com>
2022-10-25 11:21:32 -0600

    added PlacesNew without functionality in home

---
+++ src/PlacesNew.jsx
@@ -0,0 +1,33 @@
+export function PlacesNew(props) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const params = new FormData(event.target);
+    props.onCreatePlace(params, () => event.target.reset());
+  };
+
+  return (
+    <div>
+      <h1>Add a Place</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          Location Name: <input name="name" type="text" />
+        </div>
+        <div>
+          Image URL: <input name="image-url" type="text" />
+        </div>{" "}
+        <div>
+          Country: <input name="country" type="text" />
+        </div>{" "}
+        <div>
+          Latitude: <input name="latitude" type="number" />
+        </div>{" "}
+        <div>
+          Longitude: <input name="longitude" type="number" />
+        </div>{" "}
+        <div>
+          Description: <input name="description" type="text" />
+        </div>
+      </form>
+    </div>
+  );
+}
